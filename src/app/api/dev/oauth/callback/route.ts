c14d0ac2ed488d0e27da84cbc16e42ea70f64b88
Nicolas Melo <nicolasmelo12@gmail.com>
2024-04-07 01:01:37 -0300

    instagram oauth

--- src/app/api/dev/oauth/callback/route.ts
+++ src/app/api/dev/oauth/callback/route.ts
@@ -3,7 +3,7 @@ import type { NextRequest } from "next/server";
 export async function GET(request: NextRequest) {
   const url = new URL(request.url || "");
 
-  const newUrlToCall = new URL("http://localhost:3000");
+  const newUrlToCall = new URL("http://localhost:3000/api/oauth/callback");
   const searchParamEntries = Array.from(url.searchParams.entries());
   for (const [key, value] of searchParamEntries)
     newUrlToCall.searchParams.append(key, value);
