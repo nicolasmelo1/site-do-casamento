b4d8210053de32406f8eacdc1188a1958ab89ea6
Nicolas Melo <nicolasmelo12@gmail.com>
2024-04-15 12:15:37 -0300

    change photos and a few names

--- src/constants/special-persons.ts
+++ src/constants/special-persons.ts
@@ -1,7 +1,7 @@
 export const specialPersons = [
   {
     photo: "/godparents-and-bridesmaids/lu-alan.jpg",
-    name: "Lu & Alan",
+    name: "Lucy & Alan",
   },
   {
     photo: "/godparents-and-bridesmaids/edson-andrea.jpg",
@@ -17,7 +17,7 @@ export const specialPersons = [
   },
   {
     photo: "/godparents-and-bridesmaids/lucas-pati.jpeg",
-    name: "Lucas & Patricia",
+    name: "Lucas & Pati",
   },
   {
     photo: "/godparents-and-bridesmaids/vic-caio.jpg",
@@ -39,11 +39,11 @@ export const specialPersons = [
 export const moreSpecialPersons = [
   {
     photo: "/mothers-and-fathers/valter-naget.jpg",
-    name: "Valter & Naget",
+    name: "Naget & Valter",
   },
   {
     photo: "/mothers-and-fathers/lucio.jpg",
-    name: "Lúcio",
+    name: "Lußcio",
   },
   {
     photo: "/mothers-and-fathers/eliane.jpg",
@@ -51,6 +51,6 @@ export const moreSpecialPersons = [
   },
   {
     photo: "/mothers-and-fathers/giovana.jpg",
-    name: "Giovana",
+    name: "Giovanna",
   },
 ];
