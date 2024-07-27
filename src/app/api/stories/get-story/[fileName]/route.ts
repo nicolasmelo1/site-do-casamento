82d7506cbf03fb98a619f191671dac4ae276b403
Nicolas Melo <nicolasmelo12@gmail.com>
2024-07-27 18:06:49 -0300

    fetch stories

---
+++ src/app/api/stories/get-story/[fileName]/route.ts
@@ -0,0 +1,10 @@
+
+import { getStories } from "../../../../server/stories";
+import { NextRequest } from "next/server";
+
+const INSTAGRAM_STORIES_HOST = process.env.INSTAGRAM_STORIES_HOST || 'http://localhost:3001';
+
+export async function GET(request: NextRequest, { params }: { params: { fileName: string }}) {  
+    
+    return Response.redirect(`${INSTAGRAM_STORIES_HOST}/get-stories/${params.fileName}`)
+}
