bf978152c46b09fc99e0c880475d346c87940357
Nicolas Melo <nicolasmelo12@gmail.com>
2024-07-27 21:04:37 -0300

    disable caching

--- src/app/api/stories/get-story/[fileName]/route.ts
+++ src/app/api/stories/get-story/[fileName]/route.ts
@@ -3,10 +3,14 @@ import { NextRequest, NextResponse } from "next/server";
 
 const INSTAGRAM_STORIES_HOST = process.env.INSTAGRAM_STORIES_HOST || 'http://localhost:3001';
 
+export const dynamic = 'force-dynamic'
+export const revalidate = 0;
+
 export async function GET(request: NextRequest, { params }: { params: { fileName: string }}) {  
     const res = await fetch(`${INSTAGRAM_STORIES_HOST}/get-stories/${params.fileName}`);
     const blob = await res.blob();
     const headers = new Headers();
+    headers.set('Cache-Control', 'no-store')
     headers.set("Content-Type", "image/*");
     return new NextResponse(blob, { status: 200, statusText: "OK", headers });
 }
--- src/app/api/stories/read/route.ts
+++ src/app/api/stories/read/route.ts
@@ -1,14 +1,19 @@
 
 import { getStories } from "../../../../server/stories";
 
+export const dynamic = 'force-dynamic'
+export const revalidate = 0;
+
 const APP_HOST = process.env.APP_URL || 'http://localhost:3000';
 export async function GET() {
   const story = await getStories();
-  
+  const headers = new Headers();
+  headers.set('Cache-Control', 'no-store')
+
   if (story.length > 0) {
     return Response.json({
       storyUri: `${APP_HOST}/api/stories/get-story/${story[0].fileName}`
-    })
+    }, { headers })
   }
 
   return Response.json({
